b91b948fee38f9e02611eb416aaa89658952878b
Rader <raderpy@protonmail.com>
2022-12-09 11:25:26 +0100

    temporarily fix errors

--- src/entities/Entity.hpp
+++ src/entities/Entity.hpp
@@ -1,18 +1,21 @@
 #pragma once
 
 class Entity {
-    public:
-        int getHealth();
-        int getStrength();
-        Weapon getWeapon();
-        Armor getArmor();
-        void attack(Entity);
-    private:
-        int m_health;
-        int m_strength;
-        Inventory m_inventory;
-        Weapon m_weapon;
-        Armor m_armor;
-    protected:
-        Entity(int health,int strength,Inventory inventory, Weapon weapon,Armor armor);
+
+public:
+    int getHealth();
+    int getStrength();
+//    Weapon getWeapon();
+//    Armor getArmor();
+    void attack(Entity);
+
+private:
+    int m_health;
+    int m_strength;
+//    Inventory m_inventory;
+//    Weapon m_weapon;
+//    Armor m_armor;
+
+protected:
+    Entity(int health, int strength/*, Inventory inventory, Weapon weapon, Armor armor*/);
 };
