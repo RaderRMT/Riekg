e06c080afcad98c84c7ef121f1c026ee0d6caacf
Rader <raderpy@protonmail.com>
2022-12-09 15:36:12 +0100

    fix syntax

--- src/items/Item.cpp
+++ src/items/Item.cpp
@@ -1,19 +1,19 @@
 #include "Item.hpp"
 
-Item::Item(std::string name, std::string description,ItemStrategy* strategy ){
+Item::Item(std::string name, std::string description, ItemStrategy* strategy) {
     this->m_name = name;
     this->m_description = description;
     this->m_strategy = strategy;
 }
 
-void setStrategy(ItemStrategy* strategy){
+void setStrategy(ItemStrategy* strategy) {
 
 }
 
-std::string Item::getName(){
+std::string Item::getName() {
     return this->m_name;
 }
 
-std::string Item::getDescription(){
+std::string Item::getDescription() {
     return this->m_description;
 }
