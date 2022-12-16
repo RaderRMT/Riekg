3670e2ba37ebf36c7ce7a33b903b221208c4accc
Rader <raderpy@protonmail.com>
2022-12-16 12:18:27 +0100

    add operator< to fix compilation

--- src/dungeon/Position.hpp
+++ src/dungeon/Position.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
-typedef struct Position {
+typedef struct s_position {
     int x;
     int y;
     int z;
+
+    bool operator<(const s_position& position) const {
+        return this->x < position.x || this->y < position.y || this->z < position.z;
+    }
 } Position;
