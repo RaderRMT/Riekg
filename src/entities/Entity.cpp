61b4a74c88e64e2a327e38675f13c1628cad2818
Rader <raderpy@protonmail.com>
2022-12-16 12:25:55 +0100

    use armor, weapon and inventory for entities

--- src/entities/Entity.cpp
+++ src/entities/Entity.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include "Entity.hpp"
 
-Entity::Entity(int health, int strength/*, Inventory inventory, Weapon weapon, Armor armor*/) {
+Entity::Entity(int health, int strength, WeaponItem* weapon, ArmorItem* armor) {
     this->m_health = health;
     this->m_strength = strength;
-//    this->m_inventory = inventory;
-//    this->m_weapon = weapon;
-//    this->m_armor = armor;
+    this->m_inventory = new Inventory();
+    this->m_weapon = weapon;
+    this->m_armor = armor;
 }
 
 int Entity::getHealth(){
@@ -21,24 +21,32 @@ void Entity::setHealth(int health) {
     this->m_health = health;
 }
 
-/*
-Entty::getInventory(){
-    return this->m_inventory
+
+Inventory* Entity::getInventory(){
+    return this->m_inventory;
 }
 
-Entity::getWeapon(){
+WeaponItem* Entity::getWeapon(){
     return this->m_weapon;
 }
-*/
 
-/*
-Entity::getArmor(){
+void Entity::setWeapon(WeaponItem *weapon) {
+    this->m_weapon = weapon;
+}
+
+ArmorItem* Entity::getArmor(){
     return this->m_armor;
 }
-*/
 
-/*
-Entity::attack(){
-    m_weapon.attack();
+void Entity::setArmor(ArmorItem *armor) {
+    this->m_armor = armor;
 }
-*/
+
+
+void Entity::attack(Entity* target) {
+    if (this->m_weapon == nullptr) {
+        return;
+    }
+
+    this->m_weapon->attack(target);
+}
--- src/entities/Entity.hpp
+++ src/entities/Entity.hpp
@@ -1,23 +1,31 @@
 #pragma once
 
+#include "../items/ArmorItem.hpp"
+#include "../items/WeaponItem.hpp"
+#include "../items/Inventory.hpp"
+
+class WeaponItem;
+
 class Entity {
 
 public:
     int getHealth();
     void setHealth(int health);
     int getStrength();
-//    Weapon getWeapon();
-//    Inventory getInventory();
-//    Armor getArmor();
-    void attack(Entity);
+    WeaponItem* getWeapon();
+    void setWeapon(WeaponItem* weapon);
+    Inventory* getInventory();
+    ArmorItem* getArmor();
+    void setArmor(ArmorItem* armor);
+    void attack(Entity* target);
 
 private:
     int m_health;
     int m_strength;
-//    Inventory m_inventory;
-//    Weapon m_weapon;
-//    Armor m_armor;
+    Inventory* m_inventory;
+    WeaponItem* m_weapon;
+    ArmorItem* m_armor;
 
 protected:
-    Entity(int health, int strength/*, Weapon weapon, Armor armor*/);
+    Entity(int health, int strength, WeaponItem* weapon, ArmorItem* armor);
 };
