319ca45418a9f37a4b82c89532f1b2c1c95114b2
Rader <raderpy@protonmail.com>
2022-12-16 13:44:21 +0100

    use commented fields and methods

--- src/dungeon/Room.cpp
+++ src/dungeon/Room.cpp
@@ -1,16 +1,34 @@
 #include "Room.hpp"
 
 #include "Position.hpp"
-#include <cstddef>
+#include <vector>
 
-Room::Room(Position position, bool hasBed, /*std::vector<Enemy> enemies, std::vector<Item> items, */std::map<Direction, Position> exits) {
+Room::Room(Position position, bool hasBed, std::vector<Enemy> enemies, std::vector<Item> items, std::map<Direction, Position> exits) {
     this->m_position = position;
     this->m_hasBed = hasBed;
     this->m_exits = exits;
+    this->m_enemies = enemies;
+    this->m_items = items;
 }
 
 bool Room::hasBed() {
     return this->m_hasBed;
+}
+
+bool Room::hasEnemies() {
+    return this->m_enemies.empty();
+}
+
+std::vector<Enemy> Room::getEnemies() {
+    return this->m_enemies;
+}
+
+bool Room::hasItems() {
+    return this->m_items.empty();
+}
+
+std::vector<Item> Room::getItems() {
+    return this->m_items;
 }
 
 Position Room::getExit(Direction direction) {
--- src/dungeon/Room.hpp
+++ src/dungeon/Room.hpp
@@ -2,29 +2,29 @@
 
 #include <vector>
 #include <map>
-//#include "../../entities/enemy/Enemy.hpp"
-//#include "../../items/Item.hpp"
+#include "../entities/enemies/Enemy.hpp"
+#include "../items/Item.hpp"
 #include "Position.hpp"
 #include "Direction.hpp"
 
 class Room {
 
 private:
-//    std::vector<Enemy> m_enemies;
-//    std::vector<Item> m_items;
+    std::vector<Enemy> m_enemies;
+    std::vector<Item> m_items;
     std::map<Direction, Position> m_exits;
     bool m_hasBed;
     Position m_position;
 
 public:
-    Room(Position position, bool hasBed, /*std::vector<Enemy> enemies, std::vector<Item> items, */std::map<Direction, Position> exits);
+    Room(Position position, bool hasBed, std::vector<Enemy> enemies, std::vector<Item> items, std::map<Direction, Position> exits);
 
-//    bool hasEnemies();
-//    bool hasItems();
+    bool hasEnemies();
+    bool hasItems();
     bool hasBed();
 
-//    std::vector<Enemy> getEnemies();
-//    std::vector<Item> getItems();
+    std::vector<Enemy> getEnemies();
+    std::vector<Item> getItems();
     Position getExit(Direction direction);
     Position getPosition();
 };
